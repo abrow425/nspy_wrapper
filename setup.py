0564ce8870dda79318d32e09e6a8923dbad5d103
Andrew <50899169+abrow425@users.noreply.github.com>
2020-07-10 21:36:13 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 
 setup(
     name = "nspywrapper",
-    packages = ["nspy_wrapper"],
-    version = "0.1.8",
+    packages = ["nspywrapper"],
+    version = "0.1.9",
     license = "Apache",
     description = "A python wrapper for the NationStates API",
     long_description = "See here for more information: https://github.com/abrow425/nspy_wrapper",
     author = "Andrew Brown (aka SherpDaWerp)",
     author_email = "abrow425@gmail.com",
     url = "https://github.com/abrow425/nspy_wrapper",
-    download_url = "https://github.com/abrow425/nspy_wrapper/archive/v0.1.8.tar.gz",
+    download_url = "https://github.com/abrow425/nspy_wrapper/archive/v0.1.9.tar.gz",
     keywords = ["NATIONSTATES", "NS", "API", "WRAPPER"],
     classifiers = [
         "Development Status :: 3 - Alpha",
