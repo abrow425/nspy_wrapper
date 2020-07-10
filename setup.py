b3dcf5400a2cfd8082ba966132b86f674515c00b
Andrew <50899169+abrow425@users.noreply.github.com>
2020-07-10 20:44:48 +1000

    updated files to v0.1.2

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 from distutils.core import setup
 
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
 setup(
-  name = "nspy",
-  packages = ["nspy"],
-  version = "0.1.2",
-  license = "Apache",
-  description = "A python wrapper for the NationStates API",
-  author = "Andrew Brown",
-  author_email = "abrow425@gmail.com",
-  url = "https://github.com/abrow425/nspy_wrapper",
-  download_url = "https://github.com/abrow425/nspy_wrapper/archive/v_0.1.2.tar.gz",
-  keywords = ["NATIONSTATES", "NS", "API", "WRAPPER"],
-  classifiers = [
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Developers",
-    "Topic :: Software Development :: Build Tools",
-    "License :: OSI Approved :: Apache Software License",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-  ],
+    name = "nspywrapper",
+    packages = ["nspywrapper"],
+    version = "0.1.2",
+    license = "Apache",
+    description = "A python wrapper for the NationStates API",
+    long_description = long_description,
+    author = "Andrew Brown",
+    author_email = "abrow425@gmail.com",
+    url = "https://github.com/abrow425/nspy_wrapper",
+    download_url = "https://github.com/abrow425/nspy_wrapper/archive/v_0.1.2.tar.gz",
+    keywords = ["NATIONSTATES", "NS", "API", "WRAPPER"],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
